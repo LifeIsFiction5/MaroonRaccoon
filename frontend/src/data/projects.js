42e676ddbeb50189dfb5999d869505713400bec6
LifeIsFiction5 <jacob_barrett@icloud.com>
2025-12-09 23:45:16 -0600

    added projects file

---
+++ frontend/src/data/projects.js
@@ -0,0 +1,23 @@
+export const projects = [
+    {
+        id: 1,
+        name: "Swesso",
+        role: "Full-Stack Developer",
+        tech: ["React", "Spring Boot", "MySQL"],
+        description: "A web app that allows users to connect with artwork and the artists behind them through a Tinder-like swipe interface.",
+    },
+    {
+        id: 2,
+        name: "Tetrad",
+        role: "Frontend Developer",
+        tech: ["React", "Next.js", "Tailwind CSS", "Expressjs"],
+        description: "Tetrad is a construction company that specializes in fiber optic cable installation and maintenance services.",
+    },
+    {
+        id: 3,
+        name: "Personal Portfolio",
+        role: "Full-Stack Developer",
+        tech: ["React", "Next.js", "Tailwind CSS", "Node.js"],
+        description: "A personal portfolio website to showcase my projects, skills, and experience as a developer.",
+    }
+]
