6148d5d0763939b853193fb0ed97fd9dc5cce2ff
Jake Barrett <jacob_barrett@icloud.com>
2025-12-23 00:39:49 -0600

    add projects page Packet image, styling, add global horizontal gutters

--- frontend/src/app/projects/page.jsx
+++ frontend/src/app/projects/page.jsx
@@ -1,7 +1,15 @@
 export default function ProjectsPage() {
   return (
     <main style={{ padding: "2rem" }}>
-      <h1>Projects</h1>
+        <img
+            className="packet-img"
+            src="/PacketTyping.png"
+            alt="Packet typing on a laptop"
+            loading="lazy"
+        />
+
+        <div style={{ textAlign: "center" }}>
+      <span style={{color:"#610000ff", textShadow: "0 0 0.5px #e9e9e9ff, 0 0 10px #d1d1d1ff"}}><h1>Projects</h1></span>
       <br></br>
       <h3>Coming Soon: Jeff Conners Art</h3>
       <br></br>
@@ -10,6 +18,7 @@ export default function ProjectsPage() {
       <h3>Coming Soon: Swesso, the Art Discovery app</h3>
       <br></br>
       <p>You could be next on this list!</p>
+        </div>
     </main>
   );
 }
